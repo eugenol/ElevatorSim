3e38b201cd381949ba3c740819dfe8d2f128b082
TheDarkFlame <david.theo.parker@gmail.com>
2015-10-06 17:03:47 +0200

    added some classes

---
+++ ElevatorSim/BaseObject.h
@@ -0,0 +1,14 @@
+#ifndef BASEOBJECT_H
+#define BASEOBJECT_H
+
+class BaseObject{
+private:
+
+protected:
+	void update();
+	// allegrobitmap bitmap
+public:
+
+}
+
+#endif
---
+++ ElevatorSim/Panel.h
@@ -0,0 +1,4 @@
+#ifndef PANEL_H
+#define PANEL_H
+
+#endif
